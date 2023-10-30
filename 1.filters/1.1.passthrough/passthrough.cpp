4b4ed9aecf1e015d5b0d3b47fceb3a6dd24847b9
xiaofeng1990 <2840824715@qq.com>
2023-10-31 00:19:39 +0800

    add vexel_grid statistical_removal

---
+++ 1.filters/1.1.passthrough/passthrough.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <pcl/filters/passthrough.h>
+#include <pcl/point_types.h>
+
+int main()
+{
+    std::cout << "pass through filter" << std::endl;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(
+        new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(
+        new pcl::PointCloud<pcl::PointXYZ>);
+
+    // Fill in the cloud data
+    cloud->width = 5;
+    cloud->height = 1;
+    cloud->points.resize(cloud->width * cloud->height);
+
+    for (auto &point : *cloud)
+    {
+        point.x = 1024 * rand() / (RAND_MAX + 1.0f);
+        point.y = 1024 * rand() / (RAND_MAX + 1.0f);
+        point.z = 1024 * rand() / (RAND_MAX + 1.0f);
+    }
+    std::cerr << "Cloud before filtering: " << std::endl;
+    for (const auto &point : *cloud)
+        std::cerr << "    " << point.x << " " << point.y << " " << point.z
+                  << std::endl;
+
+    pcl::PassThrough<pcl::PointXYZ> pass;
+    pass.setInputCloud(cloud);
+    // 过滤z轴，可以选x，y，z
+    pass.setFilterFieldName("z");
+    // 对过滤范围取反
+    //  pass.setNegative(true);
+    // 设置过滤范围
+    pass.setFilterLimits(0.0, 1.0);
+    // 输出过滤结果
+    pass.filter(*cloud_filtered);
+    std::cerr << "Cloud after filtering: " << std::endl;
+    for (const auto &point : *cloud_filtered)
+        std::cerr << "    " << point.x << " " << point.y << " " << point.z
+                  << std::endl;
+    return 0;
+}
---
+++ 1.filters/1.2.voxel_grid/voxel_grid.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+int main()
+{
+    pcl::PCLPointCloud2::Ptr cloud(new pcl::PCLPointCloud2());
+    pcl::PCLPointCloud2::Ptr cloud_filtered(new pcl::PCLPointCloud2());
+    // Fill in the cloud data
+    pcl::PCDReader reader;
+    // load pcd file
+    reader.read("table_scene_lms400.pcd", *cloud);
+    std::cerr << "PointCloud before filtering: " << cloud->width * cloud->height
+              << " data points (" << pcl::getFieldsList(*cloud) << ")."
+              << std::endl;
+    // Create the filtering object
+    pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+    sor.setInputCloud(cloud);
+    sor.setLeafSize(0.01f, 0.01f, 0.01f);
+    // sor.setLeafSize(0.05f, 0.05f, 0.05f);
+    sor.filter(*cloud_filtered);
+
+    std::cerr << "PointCloud after filtering: "
+              << cloud_filtered->width * cloud_filtered->height
+              << " data points (" << pcl::getFieldsList(*cloud_filtered) << ")."
+              << std::endl;
+    pcl::PCDWriter writer;
+    // writer point cloud to pcd file
+    writer.write("table_scene_lms400_downsampled.pcd", *cloud_filtered,
+                 Eigen::Vector4f::Zero(), Eigen::Quaternionf::Identity(),
+                 false);
+    return 0;
+}
